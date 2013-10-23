9daf973a79dca723932c4bbe059dcc7253dbb16a
Richard Rodger <richard@ricebridge.com>
2013-10-23 14:41:06 +0100

    client ability, http hooks

--- transport.js
+++ transport.js
@@ -7,6 +7,7 @@ var buffer  = require('buffer')
 var _       = require('underscore')
 var async   = require('async')
 var connect = require('connect')
+var request = require('request')
 
 var nid = require('nid')
 
@@ -14,7 +15,25 @@ var nid = require('nid')
 module.exports = function( options ) {
   var seneca = this
   var plugin = 'transport'
+  
 
+  options.listen = parseConfig(options.listen,{
+    type:   'http',
+    host:   'localhost',
+    port:   10101,
+    path:   '/act',
+    limit:  '11mb',
+    timeout: 22222
+  })
+
+  options.client = parseConfig(options.client,{
+    type:   'http',
+    host:   'localhost',
+    port:   10101,
+    path:   '/act',
+    limit:  '11mb',
+    timeout: 22222
+  })
 
 
   options = seneca.util.deepextend({
@@ -25,23 +44,85 @@ module.exports = function( options ) {
 
 
   seneca.add({role:plugin,cmd:'listen'}, cmd_listen)
+  seneca.add({role:plugin,cmd:'client'}, cmd_client)
+
+  seneca.add({role:plugin,hook:'listen',type:'http'}, hook_listen_http)
+  seneca.add({role:plugin,hook:'client',type:'http'}, hook_client_http)
+
+
+
+
+  function parseConfig( inargs, base ) {
+    if( null == inargs ) return base;
+    if( _.isObject(inargs) && !_.isArray(inargs) ) return inargs;
+
+    var arglen = inargs.length
+
+    var config = {}
+
+    // TODO: clean this up!
+    if( 0 === arglen ) {
+      config.port = base.port
+      config.host = base.host
+      config.path = base.path
+    }
+    else if( 1 === arglen ) {
+      if( _.isObject( inargs[0] ) ) {
+        config = inargs[0]
+      }
+      else {
+        config.port = parseInt(inargs[0])
+        config.host = base.host
+        config.path = base.path
+      }
+    }
+    else if( 2 === arglen ) {
+      config.port = parseInt(inargs[0])
+      config.host = inargs[1]
+      config.path = base.path
+    }
+    else if( 3 === arglen ) {
+      config.port = parseInt(inargs[0])
+      config.host = inargs[1]
+      config.path = inargs[2]
+    }
+
+    config.port = null == config.port ? base.port : config.port 
+    config.host = null == config.host ? base.host : config.host
+    config.path = null == config.path ? base.path : config.path
+
+    config.type = null == config.type ? base.type : config.type
+
+    return config
+  }
 
 
   
   function cmd_listen( args, done ) {
     var seneca = this
 
-    var config = seneca.util.deepextend({
-      host: 'localhost',
-      port: 10101,
-      path: '/act',
-      limit: '11mb',
-      timeout: '22222'
-    },args.config||{})
+    var listen_config = parseConfig(args.listen,options.listen)
+    seneca.act( _.extend({role:plugin,hook:'listen'},options.listen,listen_config),done)
+  }
+
+
+  function cmd_client( args, done ) {
+    var seneca = this
+
+    var client_config  = parseConfig(args.client,options.client)
+    var client_args = _.extend({role:plugin,hook:'client'},options.client,client_config)
+
+    seneca.act( client_args, done )
+  }
+
+
+
+  function hook_listen_http( args, done ) {
+    var seneca = this
 
     var app = connect()
-    app.use( connect.limit( config.limit ) )
-    app.use( connect.timeout( config.timeout ) )
+    //app.use( connect.limit( args.limit ) )
+    app.use( connect.timeout( args.timeout ) )
     app.use( connect.responseTime() )
 
     // query params get injected into args
@@ -69,7 +150,7 @@ module.exports = function( options ) {
 
     
     app.use( function( req, res, next ){
-      if( 0 !== req.url.indexOf(config.path) ) return next();
+      if( 0 !== req.url.indexOf(args.path) ) return next();
 
       seneca.act( req.body, function( err, out ){
         if( err ) return next(err);
@@ -85,11 +166,33 @@ module.exports = function( options ) {
       })
     })
 
-    seneca.log.info('listen', config.host, config.port, config.path, seneca.toString())
-    var listen = app.listen( config.port, config.host )
+    seneca.log.info('listen', args.host, args.port, args.path, seneca.toString())
+    var listen = app.listen( args.port, args.host )
 
-    done(null,{listen:listen})
+    done(null,listen)
   }
+
+
+
+  function hook_client_http( args, done ) {
+    var seneca = this
+
+    var fullurl = 'http://'+args.host+':'+args.port+args.path
+
+    var client = function( args, done ) {
+      var reqopts = {
+        url:fullurl,
+        json:args
+      }
+      request.post(reqopts,function(err,response){
+        done(err, response&&response.body)
+      })
+    }
+
+    seneca.log.info('client', args.host, args.port, args.path, fullurl)
+
+    done(null,client)
+  }  
 
 
 
@@ -100,6 +203,6 @@ module.exports = function( options ) {
    */
 
   return {
-    name: plugin
+    name: plugin,
   }
 }
