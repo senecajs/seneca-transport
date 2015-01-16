4b645f8f5bb59f008c76b288dc145582066c2af5
Matteo Collina <hello@matteocollina.com>
2015-01-16 17:16:28 +0000

    Passing tests.

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 if [ ! -d "./node_modules/seneca" ]; then
-  npm install seneca
+  npm install seneca@~0.5.21
 fi
-./node_modules/.bin/mocha test/*.test.js
+
+# The published 0.2.6 release is broken, and tests
+# will fail if we do not remove it
+rm -rf node_modules/seneca/node_modules/seneca-transport
+./node_modules/.bin/mocha -R spec test/*.test.js
