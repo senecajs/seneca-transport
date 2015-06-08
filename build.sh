e6318456a09acda54a296ebec34813a4eedb442b
Richard Rodger <richard@ricebridge.com>
2015-06-08 18:54:10 +0100

    removed unneeded npm installs from build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
-if [ ! -d "./node_modules/docco" ]; then
-  npm install docco@0
-fi
-if [ ! -d "./node_modules/jshint" ]; then
-  npm install jshint@2
-fi
-
-
 ./node_modules/.bin/jshint transport.js
 ./node_modules/.bin/docco transport.js -o doc
 cp -r doc/* ../gh-pages/seneca-transport/doc
