cae84648df111a389adbf61379b9b3d6e49b9cb9
Richard Rodger <richard@ricebridge.com>
2015-06-08 18:50:19 +0100

    0.6.4 source

--- test/readme-color-client.js
+++
@@ -1,9 +0,0 @@
-
-var seneca = require('seneca')
-      
-seneca()
-  .client()
-  .act('color:red')
-
-// node readme-color-client.js --seneca.log=type:act,regex:color:red
-
--- test/service-foo.js
+++
@@ -1,7 +0,0 @@
-var type = process.argv[2]
-console.log('TYPE:'+type)
-
-require('seneca')()
-  .use('../transport.js')
-  .use('foo')
-  .listen({type:type})
