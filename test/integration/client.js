4bbaebca1f878e7c82b1821484917becd8dcd8bf
Wyatt Preul <wpreul@gmail.com>
2015-11-09 19:28:50 -0600

    Use local transport for docker

--- test/integration/client.js
+++ test/integration/client.js
@@ -1,5 +1,11 @@
-var seneca = require('seneca')
+var Transport = require('./transport')
+var Seneca = require('seneca')
 
-seneca()
+Seneca({
+    default_plugins: {
+      transort: false
+    }
+  })
+  .use(Transport)
   .client({ host: 'server', port: 8000 })
   .act('color:red', console.log)
--- test/integration/server.js
+++ test/integration/server.js
@@ -1,11 +1,17 @@
-var seneca = require('seneca')
+var Transport = require('./transport')
+var Seneca = require('seneca')
 
 var color = function () {
   this.add('color:red', function (args, callback) {
-    callback(null, { hex: '#FF0000' });
+    callback(null, { hex: '#000000' });
   })
 }
 
-seneca()
+Seneca({
+    default_plugins: {
+      transort: false
+    }
+  })
+  .use(Transport)
   .use(color)
   .listen({ port: 8000 })
