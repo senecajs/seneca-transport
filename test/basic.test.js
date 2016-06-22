a9864aeff7989710a9b3a9ce6cf85ebe27e3e491
Dean McDonnell <mcdonnelldean@outlook.com>
2016-06-22 16:38:19 +0100

    removed duplication on Basic tests

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,42 +1,32 @@
 'use strict'
 
-var Seneca = require('seneca')
+
 var Shared = require('seneca-transport-test')
-var Transport = require('../')
-var Entity = require('seneca-entity')
 var Lab = require('lab')
+var CreateInstance = require('./utils/createInstance')
 
 var lab = exports.lab = Lab.script()
 
-function createInstance (tag) {
-  var opts = {
-    default_plugins: {transport: false},
-    log: 'silent'
-  }
-
-  return Seneca(opts)
-}
-
 Shared.basictest({
-  seneca: createInstance().use(Entity).use(Transport),
+  seneca: CreateInstance(),
   script: lab,
   type: 'tcp'
 })
 
 Shared.basicpintest({
-  seneca: createInstance().use(Entity).use(Transport),
+  seneca: CreateInstance(),
   script: lab,
   type: 'tcp'
 })
 
 Shared.basictest({
-  seneca: createInstance().use(Entity).use(Transport),
+  seneca: CreateInstance(),
   script: lab,
   type: 'http'
 })
 
 Shared.basicpintest({
-  seneca: createInstance().use(Entity).use(Transport),
+  seneca: CreateInstance(),
   script: lab,
   type: 'http'
 })
