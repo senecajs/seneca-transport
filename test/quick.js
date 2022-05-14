2a5561dbf5e4359810fe62d8d2698cda4e32f982
Richard Rodger <richard@ricebridge.com>
2022-05-14 21:56:45 +0100

    quick

--- test/quick.js
+++ test/quick.js
@@ -1,26 +1,28 @@
 
 const Seneca = require('seneca')
 
-const Transport = require('../transport')
+// const Transport = require('../transport')
 
-const s0 = Seneca({ id$: 's0'})
-      .test('print')
-      .use(Transport)
-      .listen(32123)
-      .add('a:1', function a1(msg,reply) {
-        reply({x:msg.x})
-      })
+// const s0 = Seneca({ id$: 's0'})
+//       .test('print')
+//       .use(Transport)
+//       .listen(32123)
+//       .add('a:1', function a1(msg,reply) {
+//         reply({x:msg.x})
+//       })
 
-const s1 = Seneca({ id$: 's1'})
-      .test('print')
-      .use(Transport)
-      .client(32123)
+// const s1 = Seneca({ id$: 's1'})
+//       .test('print')
+//       .use(Transport)
+//       .client(32123)
 
 
-s0.ready(function(){
-  s1.ready(function(){
-    s1.act('a:1,x:99', function(err, out) {
-      console.log('RES', err, out)
-    })
-  })
-})
+// s0.ready(function(){
+//   s1.ready(function(){
+//     s1.act('a:1,x:99', function(err, out) {
+//       console.log('RES', err, out)
+//     })
+//   })
+// })
+
+
